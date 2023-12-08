da7b2cf7b2c1d99e19e2abf78daa393a3952acf0
lizhenwei <lizhenwei@toponegames.cn>
2023-12-08 18:20:49 +0800

    update: 优化注释

--- Sources/LessLocation/Expands/LessLocationAccuracy.swift
+++ Sources/LessLocation/Expands/LessLocationAccuracy.swift
@@ -10,22 +10,22 @@ import CoreLocation
 
 public enum LessLocationAccuracy {
     
-    /// 最适合导航的精度，这是最高的精度，使用更多的电量。
+    /// 最适合导航的精度，这是最高的精度，使用更多的电量
     case bestForNavigation
     
-    /// 最佳精度。
+    /// 最佳精度
     case best
     
-    /// 精确到最近的十米。
+    /// 精确到最近的十米
     case nearestTenMeters
     
-    /// 精确到最近的一百米。
+    /// 精确到最近的一百米
     case hundredMeters
     
-    /// 精确到最近的一千米。
+    /// 精确到最近的一千米
     case kilometer
     
-    /// 精确到最近的三千米。
+    /// 精确到最近的三千米
     case threeKilometers
     
 }
